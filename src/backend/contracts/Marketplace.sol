559bbe01f2ce8cf603f629e255c2fd5190de2642
Roman-Ojha <razzroman98@gmail.com>
2022-09-09 17:54:21 +0545

    feat: test for minting NFTs & makeItem function

--- src/backend/contracts/Marketplace.sol
+++ src/backend/contracts/Marketplace.sol
@@ -8,6 +8,8 @@ import "@openzeppelin/contracts/token/ERC721/IERC721.sol";
 // implement a reentrancy guard contract that will protect the marketplace from reentrancy attacks
 import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
 
+// https://docs.openzeppelin.com/contracts/4.x/api/security#ReentrancyGuard
+
 contract Marketplace is ReentrancyGuard {
     // State variable
 
@@ -15,12 +17,36 @@ contract Marketplace is ReentrancyGuard {
     address payable public immutable feeAccount; // the account that receives fees
     uint256 public immutable feePercent; // the fee percentage on sales
     // immutable means that can't change after they got assigned
+
     uint256 public itemCount;
+    // marketPlace NFTs Items
+    struct Item {
+        uint256 itemId;
+        //  Instance of the nft contract that we imported
+        IERC721 nft;
+        uint256 tokenId;
+        uint256 price;
+        address payable seller;
+        bool sold;
+    }
+    // store all the item into mapping
+    // itemId -> Item
+    mapping(uint256 => Item) public items;
 
     constructor(uint256 _feePercent) {
         // assigning to 'feeAccount' & 'feePercentage' while contract is been deployed
         feeAccount = payable(msg.sender);
         feePercent = _feePercent;
+    }
+
+    // function that make Item
+    function makeItem(
+        IERC721 _nft,
+        uint256 _tokenId,
+        uint256 _price
+    ) external nonReentrant {
+        // _nft: NFT contract instance of the nft to be listed
+        // nonReentrant: modifier prevent bad guy for calling makeItem function
     }
 
     // function to list NFTs for sale
--- src/backend/test/NFTMarketplace.test.js
+++ src/backend/test/NFTMarketplace.test.js
@@ -3,6 +3,7 @@ const { expect } = require("chai");
 describe("NFTMarketplace", function () {
   let deployer, addr1, addr2, nft, marketplace;
   let feePercent = 1;
+  let URI = "Sample URI";
 
   beforeEach(async function () {
     // first need to get contract factory
@@ -30,6 +31,21 @@ describe("NFTMarketplace", function () {
     it("Should track feeAccount and feePercent of the marketplace", async function () {
       expect(await marketplace.feeAccount()).to.equal(deployer.address);
       expect(await marketplace.feePercent()).to.equal(feePercent);
+    });
+  });
+
+  describe("Minting NFTs", function () {
+    it("Should track each minted NFT", async function () {
+      // addr1 mints an nft
+      await nft.connect(addr1).mint(URI);
+      expect(await nft.tokenCount()).to.equal(1);
+      expect(await nft.balanceOf(addr1.address)).to.equal(1);
+      expect(await nft.tokenURI(1)).to.equal(URI);
+      // addr2 mints an nft
+      await nft.connect(addr2).mint(URI);
+      expect(await nft.tokenCount()).to.equal(2);
+      expect(await nft.balanceOf(addr2.address)).to.equal(1);
+      expect(await nft.tokenURI(2)).to.equal(URI);
     });
   });
 });
