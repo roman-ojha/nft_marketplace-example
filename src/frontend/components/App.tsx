bdafbdcadf9fa59f61f9ce1fad0dc211d72e07b7
Roman-Ojha <razzroman98@gmail.com>
2022-09-10 00:34:16 +0545

    feat: added web3Handler, loadContract & create Navbar comp

--- src/frontend/components/App.tsx
+++ src/frontend/components/App.tsx
@@ -1,20 +1,77 @@
-import React from "react";
+import React, { useState } from "react";
 import logo from "./logo.png";
 import "./App.css";
 
+// Import contract ABI and its address
+import MarketplaceAbi from "../contractsData/Marketplace.json";
+import MarketplaceAddress from "../contractsData/Marketplace-address.json";
+import NFTAbi from "../contractsData/NFT.json";
+import NFTAddress from "../contractsData/NFT-address.json";
+
+// so here we will use ether to connect to metamask and metamask is connect to blockchain
+// we can create new network inside metamask to connect to hardhat development blockchain node
+// so we don't have to directly connect to blockchain from ethers.js
+// we will use metamask as ethereum provider
+// there are different type of provider but provider offered by metamask is called web3Provider
+//
+import { ethers } from "ethers";
+
+// import Component
+import Navigation from "./Navbar";
+
 function App() {
+  // account will contain the account that is connected to the app
+  const [account, setAccount] = useState(null);
+
+  // store each contract instance as state
+  const [nft, setNFT] = useState({});
+  const [marketplace, setMarketplace] = useState({});
+
+  // loading state
+  const [loading, setLoading] = useState(true);
+
+  // function to make connection to the blockchain
+  const web3Handler = async (): Promise<void> => {
+    // Get Provider from Metamask
+    const provider = new ethers.providers.Web3Provider(window.ethereum);
+    // windows.ethereum is the object injected into the browser by metamask
+
+    // Get account listed on Metamask wallet
+    const accounts = await window.ethereum.request({
+      // first account listed on list of account is the account that is connected to the app
+      method: "eth_requestAccounts",
+    });
+
+    // store connected account
+    setAccount(accounts[0]);
+
+    // get the signer of the connected account from the provider
+    const signer = provider.getSigner();
+
+    loadContract(signer);
+  };
+
+  const loadContract = async (signer: ethers.providers.JsonRpcSigner) => {
+    // function to load the contract instance
+
+    // Fetch/Get the deployed copies of contracts
+    const marketplace = new ethers.Contract(
+      MarketplaceAddress.address,
+      MarketplaceAbi.abi,
+      signer
+    );
+    // Contract(<contract_address>,<abi>,<singer>);
+    setMarketplace(marketplace);
+    const nft = new ethers.Contract(NFTAddress.address, NFTAbi.abi, signer);
+    setNFT(nft);
+
+    // set loading false when contract had loaded
+    setLoading(false);
+  };
+
   return (
     <div>
-      <nav className="navbar navbar-dark fixed-top bg-dark flex-md-nowrap p-0 shadow">
-        <a
-          className="navbar-brand col-sm-3 col-md-2 ms-3"
-          href="http://www.dappuniversity.com/bootcamp"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Dapp University
-        </a>
-      </nav>
+      <Navigation web3Handler={web3Handler} account={account} />
       <div className="container-fluid mt-5">
         <div className="row">
           <main role="main" className="col-lg-12 d-flex text-center">
